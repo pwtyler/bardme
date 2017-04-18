e1a54ad729d5427ffe31dc0e57c2c13d31ecae7e
pwtyler <philip@tylerdigital.com>
2017-04-18 11:21:12 -0400

    removed html now that formatting happening before returned from generate()

--- public/barovians.php
+++ public/barovians.php
@@ -8,7 +8,7 @@ $barovians = new Barovians();
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>When Your Bard Needs to Mock and/or Inspire</title>
+		<title>Names for all you meet in Barovia</title>
 		<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 		<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
 		<style type="text/css">
@@ -19,13 +19,7 @@ $barovians = new Barovians();
 		</style>
 	</head>
 	<body>
-		<blockquote class="text-center">
-			<h1>
-				<?php echo $barovians->generate('male'); ?>	
-			</h1>
-			<h1>
-				<?php echo $barovians->generate('female'); ?>	
-			</h1>
-		</blockquote>
+		<?php echo $barovians->generate('male'); ?>	
+		<?php echo $barovians->generate('female'); ?>	
 	</body>
 </html>
