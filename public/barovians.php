33cc99fb506118027d02bd015d6c50c4c26a442b
pwtyler <philip@tylerdigital.com>
2017-04-18 11:48:33 -0400

    broke html out to new files

--- public/barovians.php
+++ public/barovians.php
@@ -1,22 +1,7 @@
 <?php
 require_once dirname( __DIR__ ) .'/barovians.php';
 $barovians = new Barovians();
-?>
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Names for all you meet in Barovia</title>
-		<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-		<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
-		<link rel="stylesheet" type="text/css" href="../css/bardme.css">
-	</head>
-	<body>
-	<?php
-		echo $barovians->generate('male');
-		echo $barovians->generate('female');
-	?>	
-	</body>
-</html>
+include dirname( __DIR__ ) . '/html/top.php';
+echo $barovians->generate('male');
+echo $barovians->generate('female');
+include dirname( __DIR__ ) . '/html/bottom.php';
