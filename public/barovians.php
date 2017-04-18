d3e2cb92e475d4a3ca005186c2e3240b9fc881c2
pwtyler <philip@tylerdigital.com>
2017-04-18 11:57:30 -0400

    Revert "removed ! on barovian page"
    This reverts commit 241362cfd9c42b348014728379dcfed60f549570.

--- public/barovians.php
+++ public/barovians.php
@@ -3,6 +3,6 @@ require_once dirname( __DIR__ ) .'/barovians.php';
 $barovians = new Barovians();
 $title = 'Names for all you meet in Barovia';
 include dirname( __DIR__ ) . '/html/top.php';
-echo str_replace('!', '', $barovians->generate('male') );
-echo str_replace('!', '', $barovians->generate('female') );
+echo $barovians->generate('male');
+echo $barovians->generate('female');
 include dirname( __DIR__ ) . '/html/bottom.php';
